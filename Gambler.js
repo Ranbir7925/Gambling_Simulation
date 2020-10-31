eb71e24b48a23f10ca717eeb20a0d7b9621add44
Ranbir7925 <honeyranbir7925@gmail.com>
2020-10-31 21:29:17 +0530

    ADDED:Code for checking win or loose condition

--- Gambler.js
+++ Gambler.js
@@ -2,3 +2,19 @@ console.log("Welcome To Gambler Simulation")
 //Constants
 const STAKE_FOR_EVERY_DAY = 100
 const BET = 1
+//Variables
+var cash = STAKE_FOR_EVERY_DAY
+var randonValue = Math.random()
+/**
+ * Function for checking Win or Loss
+ */
+function dialyGambling(){
+    if(randonValue < 0.5){
+        console.log("Gambler Wins..!!!")
+        cash +=BET
+    }
+    else
+        console.log("Gambler Lose..!!!");    
+        cash -=BET
+}
+dialyGambling

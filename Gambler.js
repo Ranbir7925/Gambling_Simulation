3694aad97a3812f4ea6ef9f47f4fa20ca8dd26f6
Ranbir7925 <honeyranbir7925@gmail.com>
2020-10-31 21:38:54 +0530

    ADDED:Played till stake value reached to its limit

--- Gambler.js
+++ Gambler.js
@@ -1,14 +1,20 @@
 console.log("Welcome To Gambler Simulation")
+
 //Constants
 const STAKE_FOR_EVERY_DAY = 100
 const BET = 1
+const UPPER_LIMIT=STAKE_FOR_EVERY_DAY+(STAKE_FOR_EVERY_DAY * 50 /100)
+const LOWER_LIMIT=STAKE_FOR_EVERY_DAY-(STAKE_FOR_EVERY_DAY* 50 /100)
+
 //Variables
 var cash = STAKE_FOR_EVERY_DAY
-var randonValue = Math.random()
+
 /**
  * Function for checking Win or Loss
  */
 function dialyGambling(){
+    while(cash > LOWER_LIMIT && cash < UPPER_LIMIT){
+    var randonValue = Math.random()
     if(randonValue < 0.5){
         console.log("Gambler Wins..!!!")
         cash +=BET
@@ -16,5 +22,6 @@ function dialyGambling(){
     else
         console.log("Gambler Lose..!!!");    
         cash -=BET
+    }
 }
-dialyGambling
+dialyGambling()

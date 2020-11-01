61556e67f50d92b9b4c8f8b6cc70b367669ad8ea
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-01 18:09:29 +0530

    ADDED:Calculated total amount of gambler after 20 Days

--- Gambler.js
+++ Gambler.js
@@ -1,3 +1,4 @@
+
 console.log("Welcome To Gambler Simulation")
 
 //Constants
@@ -5,23 +6,40 @@ const STAKE_FOR_EVERY_DAY = 100
 const BET = 1
 const UPPER_LIMIT=STAKE_FOR_EVERY_DAY+(STAKE_FOR_EVERY_DAY * 50 /100)
 const LOWER_LIMIT=STAKE_FOR_EVERY_DAY-(STAKE_FOR_EVERY_DAY* 50 /100)
+const TOTAL_DAYS =20
 
 //Variables
-var cash = STAKE_FOR_EVERY_DAY
+var cash = 0
+var winnigCash = 0
+var totalAmount = 0
+var dictionary = {}
 
 /**
- * Function for checking Win or Loss
+ * Function for calculate daily gambling
  */
 function dialyGambling(){
+    winnigCash = 0
+    cash = STAKE_FOR_EVERY_DAY
     while(cash > LOWER_LIMIT && cash < UPPER_LIMIT){
-    var randonValue = Math.random()
-    if(randonValue < 0.5){
-        console.log("Gambler Wins..!!!")
-        cash +=BET
+        var randonValue = Math.random()
+        if(randonValue < 0.5)
+            cash +=BET
+        else
+            cash -=BET
     }
-    else
-        console.log("Gambler Lose..!!!");    
-        cash -=BET
-    }
+    winnigCash = cash - STAKE_FOR_EVERY_DAY
+    return winnigCash
 }
-dialyGambling()
+function monthlyGambling() {
+    for(day = 1;day<TOTAL_DAYS; day++){
+        var cash = dialyGambling()
+        dictionary[day] = cash
+        totalAmount += dictionary[day]
+        console.log(`Day${day} winning ammount: ${dictionary[day]}`);
+    }
+    if(totalAmount > 0 )
+        console.log("Total amount of 20 days:" +totalAmount);
+    else
+        console.log("Total amount of 20 days:" +totalAmount);
+}
+monthlyGambling()

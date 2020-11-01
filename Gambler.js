b97d9137d7acd1fab3459e3b3c01f772060adb9b
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-01 19:19:46 +0530

    ADDED:Code to give luckiest and unluckiest day

--- Gambler.js
+++ Gambler.js
@@ -30,17 +30,35 @@ function dialyGambling(){
     winnigCash = cash - STAKE_FOR_EVERY_DAY
     return winnigCash
 }
+/**
+ * Function to calculate monthly gambling
+ */
 function monthlyGambling() {
-    for(day = 1;day<TOTAL_DAYS; day++){
+    for(day = 1;day<=TOTAL_DAYS; day++){
         var cash = dialyGambling()
         dictionary[day] = cash
         totalAmount += dictionary[day]
         console.log(`Amount of Day ${day}: ${dictionary[day]}`)
-        // console.log(`Day${day} winning ammount: ${dictionary[day]}`);
     }
     if(totalAmount > 0 )
         console.log("Total amount win of 20 days:" +totalAmount);
     else
         console.log("Total amount loss of 20 days:" +totalAmount);
+    sort()       
+}
+/**
+ * For sort amount array
+ */
+function sort(){
+    var amount = Object.keys(dictionary).map(function(key){
+        return [key ,dictionary[key]]
+    })
+    amount.sort(function(first,second){
+        return second[1] - first[1]
+    });
+    //Luckiest Day
+    console.log(`Luckiest Day: ${amount.slice(0,1)}`);
+    //Unluckiest Day
+    console.log(`Unluckiest Day: ${amount.slice(amount.length-2,amount.length-1)}`);
 }
 monthlyGambling()
